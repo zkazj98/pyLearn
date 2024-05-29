c9e74eca000e5256105838199f2cab8eee3f615e
zhangkun <136334807@qq.com>
2024-05-29 17:56:10 +0800

    数据清理

---
+++ learn/dataLearn.py
@@ -0,0 +1,68 @@
+import pandas as pd  # download library to read data into dataframe
+import re
+import numpy as np
+
+pd.set_option('display.max_columns', None)
+
+recipes = pd.read_csv("../recipes.csv")
+
+# ingredients = list(recipes.columns.values)
+
+# print([match.group(0) for ingredient in ingredients for match in [(re.compile(".*(rice).*")).search(ingredient)] if match])
+# print([match.group(0) for ingredient in ingredients for match in [(re.compile(".*(wasabi).*")).search(ingredient)] if match])
+# print([match.group(0) for ingredient in ingredients for match in [(re.compile(".*(soy).*")).search(ingredient)] if match])
+print(recipes["country"].value_counts())
+column_names = recipes.columns.values
+column_names[0] = "cuisine"
+recipes.columns = column_names
+recipes.loc[recipes["cuisine"] == "austria", "cuisine"] = "austrian"
+recipes.loc[recipes["cuisine"] == "belgium", "cuisine"] = "belgian"
+recipes.loc[recipes["cuisine"] == "china", "cuisine"] = "chinese"
+recipes.loc[recipes["cuisine"] == "canada", "cuisine"] = "canadian"
+recipes.loc[recipes["cuisine"] == "netherlands", "cuisine"] = "dutch"
+recipes.loc[recipes["cuisine"] == "france", "cuisine"] = "french"
+recipes.loc[recipes["cuisine"] == "germany", "cuisine"] = "german"
+recipes.loc[recipes["cuisine"] == "india", "cuisine"] = "indian"
+recipes.loc[recipes["cuisine"] == "indonesia", "cuisine"] = "indonesian"
+recipes.loc[recipes["cuisine"] == "iran", "cuisine"] = "iranian"
+recipes.loc[recipes["cuisine"] == "italy", "cuisine"] = "italian"
+recipes.loc[recipes["cuisine"] == "japan", "cuisine"] = "japanese"
+recipes.loc[recipes["cuisine"] == "israel", "cuisine"] = "israeli"
+recipes.loc[recipes["cuisine"] == "korea", "cuisine"] = "korean"
+recipes.loc[recipes["cuisine"] == "lebanon", "cuisine"] = "lebanese"
+recipes.loc[recipes["cuisine"] == "malaysia", "cuisine"] = "malaysian"
+recipes.loc[recipes["cuisine"] == "mexico", "cuisine"] = "mexican"
+recipes.loc[recipes["cuisine"] == "pakistan", "cuisine"] = "pakistani"
+recipes.loc[recipes["cuisine"] == "philippines", "cuisine"] = "philippine"
+recipes.loc[recipes["cuisine"] == "scandinavia", "cuisine"] = "scandinavian"
+recipes.loc[recipes["cuisine"] == "spain", "cuisine"] = "spanish_portuguese"
+recipes.loc[recipes["cuisine"] == "portugal", "cuisine"] = "spanish_portuguese"
+recipes.loc[recipes["cuisine"] == "switzerland", "cuisine"] = "swiss"
+recipes.loc[recipes["cuisine"] == "thailand", "cuisine"] = "thai"
+recipes.loc[recipes["cuisine"] == "turkey", "cuisine"] = "turkish"
+recipes.loc[recipes["cuisine"] == "vietnam", "cuisine"] = "vietnamese"
+recipes.loc[recipes["cuisine"] == "uk-and-ireland", "cuisine"] = "uk-and-irish"
+recipes.loc[recipes["cuisine"] == "irish", "cuisine"] = "uk-and-irish"
+
+# get list of cuisines to keep
+recipes_counts = recipes["cuisine"].value_counts()
+cuisines_indices = recipes_counts > 50
+print(cuisines_indices)
+cuisines_to_keep = list(np.array(recipes_counts.index.values)[np.array(cuisines_indices)])
+rows_before = recipes.shape[0]  # number of rows of original dataframe
+print("Number of rows of original dataframe is {}.".format(rows_before))
+
+recipes = recipes.loc[recipes['cuisine'].isin(cuisines_to_keep)]
+
+rows_after = recipes.shape[0]  # number of rows of processed dataframe
+print("Number of rows of processed dataframe is {}.".format(rows_after))
+
+print("{} rows removed!".format(rows_before - rows_after))
+recipes = recipes.replace(to_replace="Yes", value=1)
+recipes = recipes.replace(to_replace="No", value=0)
+check_recipes = recipes.loc[
+    (recipes["rice"] == 1) &
+    (recipes["soy_sauce"] == 1) &
+    (recipes["wasabi"] == 1) &
+    (recipes["seaweed"] == 1)
+]
